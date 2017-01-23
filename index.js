4b51b52c734f714b215a00da025a39840403ce3b
Tommy McGahee <tommy.mcgahee@usg.edu>
2017-01-22 21:04:25 -0500

    example of nearly working diffing with underscore utility library. trying out lodash next.

--- index.js
+++ index.js
@@ -1,102 +1,69 @@
 // TODO
 
-// fix data structure of jobResponse
-// diff data for changes
 // write query to json file
 
 var request    = require('request'),
     cheerio    = require('cheerio'),
     fs         = require('fs'),
+    _          = require('underscore'),
+    url        = 'http://usg.hiretouch.com/browse-jobs/all-jobs?&start=1&per=100',
     jobFile,
     jobResponse;
 
 const http     = require('http');
 const hostname = '127.0.0.1';
 const port     = 3000;
+// const server   = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.write(JSON.stringify(jobResponse));
+//     res.end();
+// });
+// server.listen(port, hostname, () => {
+//     console.log(`Server running at http://${hostname}:${port}/`);
+// });
 
-const server   = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    // res.write(jobResponse);
-    // res.end();
-});
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+function areDifferentByProperty(o, n, prop) {
+    var oldJobs = o.map( function(x) {
+        return x[prop];
+    });
+    var newJobs = n.map( function(x) {
+        return x[prop];
+    });
 
-function finished() {
-
-
-
-
+    // var diff    = _.difference(oldJobs, newJobs);
 }
 
-// read previous jobs and store in memory
-fs.readFile('jobstash.json', handleFile);
-
-// callback
-function handleFile(error, file) {
+fs.readFile('jobstash.json', function(error, file) {                            // read previous jobs
     if (error) return console.error('There was an error: ' + error);
-    jobFile = file.toString();
+    file.toString();                                                            // turn buffer into string
+    jobFile = JSON.parse(file);                                                 // turn string into JS object and save to global var
 
-    // request is nested in fs callback for async
-    request({
-            url: 'http://usg.hiretouch.com/browse-jobs/all-jobs?&start=1&per=100'
-          }, handleResponse);
-}
+    request(url, function(error, response, html) {
+        if (!error && response.statusCode == 200) {
+            var $    = cheerio.load(html),
+                jobs = [];
+            $('li.job').each(function(i, el) {
+                var href  = $(this).find('h4 > a').attr('href'),
+                    id    = href.substring( href.indexOf('jobID=') + 6, href.indexOf('&job=') ),
+                    title = $(this).find('h4 > a').text(),
+                    desc  = $(this).find('.short_description p:not(:first-child)').text(),
+                    dept  = $(this).find('.department span').text(),
+                    loc   = $(this).find('.location span').text(),
+                    job   = {
+                        job_id: id,
+                        title: title,
+                        description: desc,
+                        department: dept,
+                        location: loc
+                    };
+                    jobs.push(job);
+            });
+            jobResponse    = jobs;
+            areDifferentByProperty(jobFile, jobResponse, 'job_id');
 
-// callback
-function handleResponse(error, response, html) {
-    if (!error && response.statusCode == 200) {
-        var $    = cheerio.load(html),
-            jobs = [];
-        $('li.job').each(function(i, el) {
-            var title = $(this).find('h4 > a').text(),
-                desc  = $(this).find('.short_description p:not(:first-child)').text(),
-                dept  = $(this).find('.department span').text(),
-                loc   = $(this).find('.location span').text(),
-                job   = {
-                    title: title,
-                    description: desc,
-                    department: dept,
-                    location: loc
-                };
-                jobs.push(job);
-        });
-        jobResponse = JSON.stringify(jobs);
-
-        finished();
-
-    } else {
-        console.log('There was an error: ' + error);
-    }
-}
-
-// function scrapeRequest(res) {
-//     return request({
-//        url: 'http://usg.hiretouch.com/browse-jobs/all-jobs?&start=1&per=100'
-//     }, function (error, response, html) {
-//           if (!error && response.statusCode == 200) {
-//               var $    = cheerio.load(html),
-//                   jobs = [];
-//               $('li.job').each(function(i, el) {
-//                   var title = $(this).find('h4 > a').text(),
-//                       desc  = $(this).find('.short_description p:not(:first-child)').text(),
-//                       dept  = $(this).find('.department span').text(),
-//                       loc   = $(this).find('.location span').text(),
-//                       job   = {
-//                           title: title,
-//                           description: desc,
-//                           department: dept,
-//                           location: loc
-//                       };
-//                       jobs.push(job);
-//               });
-//               // console.log(jobs);
-//               res.write( JSON.stringify(jobs) );
-//           } else {
-//               console.log("We’ve encountered an error: " + error);
-//           }
-//           res.end();
-//     });
-// }
+        } else {
+            console.log('There was an error: ' + error);
+        }
+    });
+});
